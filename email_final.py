6a543c57f51c58601b676e180cf3614049d71071
vijit2k7 <vijit2k7@yahoo.in>
2019-10-10 23:52:03 +0530

    Update email_final.py

--- email_final.py
+++ email_final.py
@@ -67,7 +67,7 @@ stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
 
 
 # Convert email body to list
-data = email_df.toValue()
+data = email_df.values
 # tokenize - break down each sentence into a list of words
 def sent_to_words(sentences):
     for sentence in sentences:
