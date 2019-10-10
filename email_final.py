77c1d2f46b25b78a0b44228614a1aad2216fa89d
vijit2k7 <vijit2k7@yahoo.in>
2019-10-10 20:55:03 +0530

    Update email_final.py

--- email_final.py
+++ email_final.py
@@ -2,21 +2,31 @@ import pyLDAvis
 import pyLDAvis.gensim
 import matplotlib.pyplot as plt
 import pandas as pd
-#emails = pd.read_csv('data/mail-body-CSV.csv',skiprows=2)
+filepath = 'data/mail-body-CSV.csv'
+emails=[]
+with open(filepath ,encoding="utf8") as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print("Line {}: {}".format(cnt, line.strip()))
+       emails.append(line)
+       line = fp.readline()
+       cnt += 1
+# emails = pd.read_csv('data/mail-body-CSV.csv',skiprows=2)
 #print("emails at start",emails.loc[[0]])
 from google.cloud import bigquery
 import json
 
-client = bigquery.Client()
-query_job = client.query("""
-        SELECT Corps
-        FROM `paris-ilab.paris-pune-hackathon.mails`
-        LIMIT 1000""")
-
-emails = query_job.result()  # Waits for job to complete.
-for row in emails:
-    print(row.DeNom)
-    print(row.ANom)
+# client = bigquery.Client()
+# query_job = client.query("""
+#         SELECT Corps
+#         FROM `paris-ilab.paris-pune-hackathon.mails`
+#         LIMIT 1000""")
+#
+# emails = query_job.result()  # Waits for job to complete.
+# for row in emails:
+#     print(row.DeNom)
+#     print(row.ANom)
 
 
 def parse_raw_message(raw_message):
@@ -32,17 +42,16 @@ def parse_raw_message(raw_message):
 
 
 def parse_into_emails(emails):
+    print("email type is",type(emails))
     emails_new=[]
-    for index,row in emails.iterrows():
-        print("each row",index,row)
-        # emails_new.append(parse_raw_message(emails[[index]]))
+    for email in emails:
+        print("each row",email)
+        emails_new.append(parse_raw_message(email))
     print("emails are",emails_new)
     return {
         'body': emails
     }
-
 email_df = pd.DataFrame(parse_into_emails(emails))
-print("HEAD OF EMAIL",email_df,email_df.head())
 
 import re
 import numpy as np
@@ -67,7 +76,6 @@ stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
 
 
 # Convert email body to list
-print("email_df",email_df.body)
 data = email_df.body.values.tolist()
 print("data is",data)
 # tokenize - break down each sentence into a list of words
@@ -159,12 +167,12 @@ doc_lda = lda_model[corpus]
 # Compute Perplexity
 print('\nPerplexity: ', lda_model.log_perplexity(corpus))  # a measure of how good the model is. lower the better.
 
-# Compute Coherence Score
-coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
-coherence_lda = coherence_model_lda.get_coherence()
-print('\nCoherence Score: ', coherence_lda)
+# # Compute Coherence Score
+# coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+# coherence_lda = coherence_model_lda.get_coherence()
+# print('\nCoherence Score: ', coherence_lda)
 
-# Visualize the topics
-pyLDAvis.enable_notebook(sort=True)
-vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
-pyLDAvis.display(vis)
+# # Visualize the topics
+# pyLDAvis.enable_notebook(sort=True)
+# vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
+# pyLDAvis.display(vis)
